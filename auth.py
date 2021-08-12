57110ac5df57749f721cf461c570ffe3850d34ce
Facundo Diaz <70913784+facu2279@users.noreply.github.com>
2021-08-12 11:00:29 -0300

    modificacion url endpoints, add comments y se generaron dicts con datos de pruebas hasta generar database

--- auth.py
+++ auth.py
@@ -1,42 +1,79 @@
+from datetime import datetime
 import pyotp
 from flask import *
 
-
 app = Flask(__name__)
 
+usuarios = {
+    "caraque":"2U5EFSHZEYUE5KLL56H2DASNLMUT3HJN",
+    "fadiaz":"XAD7IP5YHH5S7ML5DPZJI55D6JBLSKEY",
+    "pepe":"WN3TNXM75D5N234NHFTDXSDFCGWRYCTM",
+    "scalvete":"AE7ZT2QLKLY2WR4NJM3ID3FG3O5BGNSF",
+    "TESTING": "VLGAJEKSHSQ4HOYNLOX4TT6CYNBXZLRF"
+    }
 
 # homepage route
 @app.route("/")
 def index():
-    return "<h1>Hello World!</h1>"
+    return "Online"
 
 """
     Recive por parametros la secret key y el pin a validar
     Returna true or false
 """
-@app.route("/lorenzo/validate_pin", methods=["GET", "POST"])
+@app.route("/lorenzo/validar_pin", methods=["GET", "POST"])
 def a():
-    secret_key_user = str(request.args.get('sec_key'))
+    """ Hacer consulta a base de datos con el nombre de usuario para poder extraer secret key
+    """
+    user = str(request.args.get('user'))
     pin = str(request.args.get('pin'))
-    if (secret_key_user != None and pin != None):
-        totp = pyotp.TOTP(secret_key_user)
-        return str(totp.verify(pin))
+    if user in usuarios.keys():
+        secret_key_user = usuarios[user]
+        if pin:
+            if (secret_key_user):
+                totp = pyotp.TOTP(secret_key_user)
+                return str(totp.verify(pin))
+        else:
+            return "Ingrese un pin a validar"
     else:
-        return "Error"
+        return "Usuario no valido"
 
 """
 Recibe el user por url y genero el string para que creen el qr
+
 En issuer_name va el nombre de la app que va aparecer en la aplicacion authenticator
+
+En dict_to_bdd se guarda, nombre del usuario, secret key, fecha y qr para guardar en base de datos
+
+Retorno un string para generar qr
 """
-@app.route("/lorenzo/generar_user", methods=["GET", "POST"])
+@app.route("/lorenzo/generar_usuario", methods=["GET", "POST"])
 def b():
-    dict = {}
+    dict_to_bdd = {}
     user = str(request.args.get('user'))
-    secret_key = pyotp.random_base32()
-    dict['secret_key'] = str(secret_key)
-    dict['codigo_para_qr']= pyotp.totp.TOTP(secret_key).provisioning_uri(name=user, issuer_name="Lorenzo")
-    return dict
+    if user in usuarios.keys():
+        return "Este usuario ya tiene un qr activo"
+    else:
+        secret_key = pyotp.random_base32()
+        qr = str(pyotp.totp.TOTP(secret_key).provisioning_uri(name=user, issuer_name="Lorenzo"))
+        dict_to_bdd['user'] = user
+        dict_to_bdd['secret_key'] = str(secret_key)
+        dict_to_bdd['qr'] = qr
+        fecha = datetime.now()
+        fecha = fecha.strftime("%Y-%m-%d")
+        dict_to_bdd['fecha'] = fecha
+        #dejar esto para poder tener usuarios registrados hasta que no se agregue bdd
+        usuarios[user] = secret_key
+        return qr
+
+@app.route("/lorenzo/print", methods=["GET", "POST"])
+def c():
+    return usuarios
 
 # running flask server
 if __name__ == "__main__":
-    app.run(host='0.0.0.0' , port=8080)
+    # para correr en local host usar este
+    app.run(host='0.0.0.0')
+    # para correr en servidor usar este
+    #app.run(host='0.0.0.0, port=8080)
+    
