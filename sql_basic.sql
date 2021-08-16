131a4ae0fe9d42bc489df7b5c72fd196bb417bf9
Facundo Diaz <70913784+facu2279@users.noreply.github.com>
2021-08-16 12:01:11 -0300

    Update sql_basic.sql

--- sql_basic.sql
+++ sql_basic.sql
@@ -12,8 +12,7 @@ CREATE TABLE `usuarios` (
 
 
 INSERT INTO `usuarios` (`id`, `usuario`, `password`) VALUES
-(1, 'fadiaz@hola.com', 'qjwehhaxxa'),
-(2, 'caraque@hola.com', 'eiy123');
+(1, 'fadiaz@hola.com', 'qjwehhaxxa');
 
 
 CREATE TABLE `usuarios_qr` (
@@ -26,8 +25,7 @@ CREATE TABLE `usuarios_qr` (
 
 
 INSERT INTO `usuarios_qr` (`id`, `usuario`, `secret_key`, `qr`, `fecha`) VALUES
-(29, 'fadiaz', 'SLJPDIXWL3VBFPTTGWECMV5JAUELNF3O', 'otpauth://totp/App_Testing:fadiaz?secret=SLJPDIXWL3VBFPTTGWECMV5JAUELNF3O&issuer=App_Testing', '2021-08-13'),
-(30, 'caraque', 'I5XOQ4IAVM2EWAJYBJHDGL2PW3CIF2WS', 'otpauth://totp/App_Testing:caraque?secret=I5XOQ4IAVM2EWAJYBJHDGL2PW3CIF2WS&issuer=App_Testing', '2021-08-13');
+(29, 'fadiaz', 'SLJPDIXWL3VBFPTTGWECMV5JAUELNF3O', 'otpauth://totp/App_Testing:fadiaz?secret=SLJPDIXWL3VBFPTTGWECMV5JAUELNF3O&issuer=App_Testing', '2021-08-13');
 
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`id`);
